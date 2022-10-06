c1eb4fd08502826bd3460579418baeb1279f6a66
Ron <31913631+OverAny@users.noreply.github.com>
2022-10-06 04:03:49 -0500

    test

--- src/CoolForm.js
+++ src/CoolForm.js
@@ -5,66 +5,70 @@ import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 
 const CustomForm = ({ status, message, onValidated }) => {
-        let email, name;
-        const submit = () =>
+    let email;
+    const submit = () =>
         email &&
-        name &&
         email.value.indexOf("@") > -1 &&
         onValidated({
             EMAIL: email.value,
-            NAME: name.value
         });
-    
-        return (
+
+    return (
         <div className="">
-            {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-            {status === "error" && (
-            <div
-                style={{ color: "red" }}
-                dangerouslySetInnerHTML={{ __html: message }}
-            />
-            )}
-            {status === "success" && (
-            <div
-                style={{ color: "green" }}
-                dangerouslySetInnerHTML={{ __html: message }}
-            />
-            )}
-
-
+        
             <input
-            className="CF-input" 
-            ref={node => (email = node)}
-            type="email"
-            placeholder="your@example.com"
+                className="CF-input"
+                ref={node => (email = node)}
+                type="email"
+                placeholder="your@example.com"
             />
             <button className="CF-button" onClick={submit}>
-            Join
+                Join
             </button>
+            {status === "sending" && <div style={{ fontSize: 20,color: "white" }}>sending...</div>}
+            {status === "error" && (
+                <div
+                    style={{ fontSize: 20, color: "red" }}
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
+            {status === "success" && (
+                <div
+                    style={{ fontSize: 20, color: "white" }}
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
+
+
         </div>
-        );
-    };
+    );
+};
 
 
 class CoolForm extends Component {
     render() {
-        const url = 
-        "https://uiuc.us10.list-manage.com/subscribe/post?u=50dafeb09b4fb2ae36474db4c&amp;id=761087a979&amp;f_id=004630e2f0";
+        const url =
+            "https://uiuc.us10.list-manage.com/subscribe/post?u=50dafeb09b4fb2ae36474db4c&amp;id=761087a979&amp;f_id=004630e2f0";
         return (
-        <div>
-            <MailchimpSubscribe
-            url={url}
-            render={({ subscribe, status, message }) => (
-                <CustomForm
-                status={status}
-                message={message}
-                onValidated={formData => subscribe(formData)}
+            <div>
+
+                <MailchimpSubscribe
+                    url={url}
+
+                    render={({ subscribe, status, message }) => (
+                        <div>
+                            <CustomForm
+                                status={status}
+                                message={message}
+                                onValidated={formData => subscribe(formData)}
+                            />
+                        </div>
+                    )}
+
                 />
-            )}
-            />
-        </div>
+            </div>
         );
     }
-    }
-    
+}
+
 export default CoolForm
