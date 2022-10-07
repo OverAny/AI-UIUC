03c9e17db0b3a33dc7f203b3eb2cffc6b0b295d7
Noratho <60336216+Noratho@users.noreply.github.com>
2022-10-06 21:12:04 -0500

    New header added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import './App.css';
-import logo from "./images/logo2.png";
-import background from "./images/backgroundLongTwo.png";
-import MailchimpSubscribe from "react-mailchimp-subscribe"
-import CoolForm from "./CoolForm.js"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-const url = "https://uiuc.us10.list-manage.com/subscribe/post?u=50dafeb09b4fb2ae36474db4c&amp;id=761087a979&amp;f_id=004630e2f0";
 
-// simplest form (only email)
-const SimpleForm = () => <MailchimpSubscribe url={url}/>
+import Homepage from "./views/Homepage";
+import "./App.css"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header style={{backgroundImage: `url(${background})` , backgroundSize: "cover", backgroundPosition: "center" }}className="App-header">
-        <div className='App-headerbar'>
-            <div className='App-logodiv'>
-              <img src={logo} className="App-logo" alt="logo" />
-            </div>
-        </div>
-          <div className="App-maindiv" style={{paddingBottom: 100}}>
-          {/* style={{width: 1000, fontSize: 80, paddingBottom: 0, 
-            fontFamily: "Sansation-Light", 
-            fontWeight: 100, 
-            letterSpacing: 3,
-            display: "inline-block"}} */}
-          <p className="App-title">
-          Artificial Intelligence <br/>
-          is Coming to Illinois
-          </p>
-
-
-          <CoolForm/>
-
-        </div>
-      </header>
+        
+        <Router>
+                <Routes>
+                    <Route exact path="/" element={<Homepage/>} />
+                </Routes>
+        </Router>
     </div>
   );
 }
-
-export default App;
---
+++ src/views/Homepage.js
@@ -0,0 +1,57 @@
+import "./../App.css";
+import "./Homepage.css";
+import logo from "./../images/logo2.png";
+import background from "./../images/backgroundLongTwo.png";
+import { Link } from "react-router-dom";
+import CoolForm from "./../CoolForm.js";
+
+const url =
+  "https://uiuc.us10.list-manage.com/subscribe/post?u=50dafeb09b4fb2ae36474db4c&amp;id=761087a979&amp;f_id=004630e2f0";
+
+function App() {
+  return (
+    <div className="App">
+      <header
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="App-header"
+      >
+        <div className="App-headerbar">
+          <div className="App-logodiv">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="HP-header-right">
+            <a
+              className="HP-invovled-href"
+              href="https://illinois.campuslabs.com/engage/organization/ai"
+              target="_blank"
+            >
+              <button className="HP-involved">Involved@Illinois</button>
+            </a>
+            <a href="mailto:team@uiuc.ai?subject=Subject&body=Body%20goes%20here">
+              <button className="HP-contact">Contact</button>
+            </a>
+          </div>
+        </div>
+        <div className="App-maindiv" style={{ paddingBottom: 100 }}>
+          {/* style={{width: 1000, fontSize: 80, paddingBottom: 0, 
+            fontFamily: "Sansation-Light", 
+            fontWeight: 100, 
+            letterSpacing: 3,
+            display: "inline-block"}} */}
+          <p className="App-title">
+            Artificial Intelligence <br />
+            is Coming to Illinois
+          </p>
+
+          <CoolForm />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
